68e3ab108330604111b4cc9a938a191a34f048a7
sezeresim <sezeresim@gmail.com>
2020-05-26 16:45:34 +0300

    account

--- src/core/url.js
+++ src/core/url.js
@@ -3,4 +3,5 @@ const LOGIN_URL = 'http://sezer.herokuapp.com/api/auth/login';
 const REGISTER_URL = BASE_URL + '/auth/register';
 const HOME_URL = BASE_URL + '/home';
 const PROFILE_URL = BASE_URL + '/auth/getuser';
-export default {LOGIN_URL, REGISTER_URL, HOME_URL, PROFILE_URL};
+const ACCOUNT_URL = BASE_URL + '/personal/';
+export default {LOGIN_URL, REGISTER_URL, HOME_URL, PROFILE_URL, ACCOUNT_URL};
--- src/screens/Account/Account.js
+++ src/screens/Account/Account.js
@@ -1,25 +1,30 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState, useEffect, useContext} from 'react';
+import {StyleSheet, View, FlatList} from 'react-native';
 import {Text, DataTable} from 'react-native-paper';
 import color from '../../core/colors';
 import AwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import HTTP from '../../core/url';
 import Axios from 'axios';
+import {AuthContext} from '../../context/AuthContext';
 
 const Account = ({navigation}) => {
+  const authContext = useContext(AuthContext);
   const [accountData, setAccountData] = useState([]);
+  const id = authContext.userData.id;
 
   useEffect(() => {
     getAccountData();
   }, []);
 
-  const getAccountData = async callback => {
-    await Axios.get(HTTP.ACCOUNT_URL)
+  const getAccountData = () => {
+    Axios.get(HTTP.ACCOUNT_URL + id)
       .then(res => {
-        setAccountData(res.data.data.reverse());
+        setAccountData(res.data.data);
+        console.log(res.data.data);
       })
       .catch(err => alert(err));
   };
+
   return (
     <View style={styles.View}>
       <DataTable>
@@ -28,22 +33,36 @@ const Account = ({navigation}) => {
           <DataTable.Title numeric>Son Tarih</DataTable.Title>
           <DataTable.Title numeric>Durum</DataTable.Title>
         </DataTable.Header>
+        <FlatList
+          data={accountData}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => (
+            <DataTable.Row>
+              <DataTable.Cell>{item.title}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.last_date}</DataTable.Cell>
+              <DataTable.Cell numeric>
+                {item.complete ? (
+                  <AwesomeIcon size={25} style={styles.Check} name={'check'} />
+                ) : (
+                  <AwesomeIcon
+                    size={25}
+                    style={styles.Wait}
+                    name={'hourglass-end'}
+                  />
+                )}
+              </DataTable.Cell>
+            </DataTable.Row>
+          )}
+        />
 
-        <DataTable.Row>
-          <DataTable.Cell>{accountData}</DataTable.Cell>
-          <DataTable.Cell numeric>159</DataTable.Cell>
-          <DataTable.Cell numeric>
-            <AwesomeIcon size={25} style={styles.Check} name={'check'} />
-          </DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Pagination
+        {/* <DataTable.Pagination
           page={1}
           numberOfPages={3}
           onPageChange={page => {
             console.log(page);
           }}
           label="1-2 of 6"
-        />
+        /> */}
       </DataTable>
     </View>
   );
